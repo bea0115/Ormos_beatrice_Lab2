4dff1819b373ba546d11f5f36a98fc16a24c0560
Beatrice Ormos <madalinaormos@yahoo.com>
2024-10-19 21:18:32 +0300

    Add project files.

---
+++ Data/Ormos_beatrice_Lab2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ormos_beatrice_Lab2.Models;
+
+namespace Ormos_beatrice_Lab2.Data
+{
+    public class Ormos_beatrice_Lab2Context : DbContext
+    {
+        public Ormos_beatrice_Lab2Context (DbContextOptions<Ormos_beatrice_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ormos_beatrice_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Ormos_beatrice_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
